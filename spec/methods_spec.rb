73929de530817c855d841afeede75833522d89ea
Sergio <sergioobolevich@gmail.com>
2020-10-22 14:58:24 -0300

    Add tests for country_stats rspec

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -10,15 +10,15 @@ describe '#world_stats' do
     expect(wstats.class).to be(Array)
   end
 
-  it 'array has 3 integer' do
+  it 'array has integer in pos [0]' do
     expect(wstats[0].class).to be(Integer)
   end
 
-  it 'array has 3 integer' do
+  it 'array has integer in pos [1]' do
     expect(wstats[1].class).to be(Integer)
   end
 
-  it 'array has 3 integer' do
+  it 'array has integer in pos [2]' do
     expect(wstats[2].class).to be(Integer)
   end
 
@@ -26,16 +26,32 @@ describe '#world_stats' do
     expect(wstats.length).to eql 3
   end
 
-  it 'array has no strings' do
-    expect(wstats[0]).to not_be(String)
+  it 'array has no strings on pos [0]' do
+    expect(wstats[0]).to_not be(String)
   end
 
-  it 'array has no strings' do
-    expect(wstats[1]).to not_be(String)
+  it 'array has no strings on pos [1]' do
+    expect(wstats[1]).to_not be(String)
   end
 
-  it 'array has no strings' do
-    expect(wstats[2]).to not_be(String)
+  it 'array has no strings on pos [2]' do
+    expect(wstats[2]).to_not be(String)
+  end
+
+end
+
+describe '#country_stats' do
+  # let(:country_i) { country_stats }
+  it 'return an array when pass a number 1' do
+    expect(country_stats(1).class).to be(Array)
+  end
+
+  it 'return an array when pass a number 5' do
+    expect(country_stats(5).class).to be(Array)
+  end
+
+  it 'return an array when pass a string "argentina"' do
+    expect(country_stats('argentina').class).to be(Array)
   end
 
 end
