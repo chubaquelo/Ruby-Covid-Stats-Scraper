034f3c16359cbee62fae641566d8cfdc893d9988
Sergio <sergioobolevich@gmail.com>
2020-10-22 14:52:25 -0300

    Add world_stats rspec tests

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -21,6 +21,23 @@ describe '#world_stats' do
   it 'array has 3 integer' do
     expect(wstats[2].class).to be(Integer)
   end
+
+  it 'array is lenght of 3' do
+    expect(wstats.length).to eql 3
+  end
+
+  it 'array has no strings' do
+    expect(wstats[0]).to not_be(String)
+  end
+
+  it 'array has no strings' do
+    expect(wstats[1]).to not_be(String)
+  end
+
+  it 'array has no strings' do
+    expect(wstats[2]).to not_be(String)
+  end
+
 end
 
 # rubocop:enable Style/FrozenStringLiteralComment
